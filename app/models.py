7f31f183159152b657037dc717254e3eeab1f318
limu2030 <limu2030@gmail.com>
2022-06-15 04:56:18 +0300

    ft: create the project model

--- app/models.py
+++ app/models.py
@@ -12,3 +12,40 @@ class Profile(models.Model):
     username =models.CharField(max_length=100 , null=True)
     bio=models.TextField(null=True)
     contacts=models.CharField(max_length=300)
+
+def __str__(self):
+        return self.user.username
+    
+    
+def save_profile(self):
+        self.save()
+        
+        
+def delete_profile(self):
+        self.delete()
+        
+def update_profile(self):
+        self.update()    
+
+class Project(models.Model):
+    title=models.CharField(max_length=100, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True, blank=True )
+    post_image=CloudinaryField('post_image')
+    description=models.TextField(null=False)
+    url = models.URLField(null=True)
+    company = models.CharField(max_length=200, blank=True)
+    languages = models.CharField(max_length=100, blank=True)
+    posted_at=models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, null = True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+    
+    def save_project_post(self):
+        self.save()
+        
+    def delete_project_post(self):
+        self.delete()
+        
+    def update_project_post(self):
+        self.update()
