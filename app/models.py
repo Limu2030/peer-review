928cd2da582f888ff4f568cc6c5d77d98b194b06
limu2030 <limu2030@gmail.com>
2022-06-15 04:54:42 +0300

    ft: create the profile model

--- app/models.py
+++ app/models.py
@@ -4,3 +4,11 @@ from cloudinary.models import CloudinaryField
 from django.forms import URLField
 
 # Create your models here.
+
+# the profile model
+class Profile(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='profile')
+    profile_pic=CloudinaryField('image')
+    username =models.CharField(max_length=100 , null=True)
+    bio=models.TextField(null=True)
+    contacts=models.CharField(max_length=300)
