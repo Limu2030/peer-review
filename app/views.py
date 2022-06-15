4b71c05b2c8e8d767e342229038aa517166f2b47
limu2030 <limu2030@gmail.com>
2022-06-15 04:08:16 +0300

    ft: create the register view

--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def register(request):
+    return render(request, 'register.html')
